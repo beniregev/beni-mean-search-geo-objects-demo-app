3f3288b0577438d30e0e7c9be072d6dfe77b8bc0
binyamin.regev <beniregev@gmail.com>
2024-11-04 23:46:37 +0200

    Create SearchWidgetComponent - 4 files - Initial

---
+++ client-angular17/src/app/component/widget/search-widget/search-widget.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-widget',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-widget.component.html',
+  styleUrl: './search-widget.component.css'
+})
+export class SearchWidgetComponent {
+
+}
