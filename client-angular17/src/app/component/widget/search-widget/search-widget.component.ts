6990dfa562651517afc97a8658182e5eec5d57b7
binyamin.regev <beniregev@gmail.com>
2024-11-05 00:02:08 +0200

    Add HeaderComponent to SearchWidgetComponent - update "search-widget.component.ts" and "search-widget.component.html" files

--- client-angular17/src/app/component/widget/search-widget/search-widget.component.ts
+++ client-angular17/src/app/component/widget/search-widget/search-widget.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../partial/header/header.component';
 
 @Component({
   selector: 'app-search-widget',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './search-widget.component.html',
-  styleUrl: './search-widget.component.css'
+  styleUrl: './search-widget.component.css',
 })
-export class SearchWidgetComponent {
-
-}
+export class SearchWidgetComponent {}
