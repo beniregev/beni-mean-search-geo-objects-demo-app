570ec89430de66e8da0badaf2b1eeea7bf96af00
binyamin.regev <beniregev@gmail.com>
2024-11-05 00:03:05 +0200

    Add SearchWidgetComponent to "app.component.ts" and "app.component.html" files

--- client-angular17/src/app/app.component.ts
+++ client-angular17/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchWidgetComponent } from './component/widget/search-widget/search-widget.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchWidgetComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'client-angular17';
